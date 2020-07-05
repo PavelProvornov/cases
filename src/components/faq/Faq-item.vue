c372d854eecd36c1a50d3151ddf3ad7adacc3d65
Pavel <pavelprovornov@gmail.com>
2020-07-05 15:10:55 +0300

    Add faq

---
+++ src/components/faq/Faq-item.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="faq__list-item faq-item" :class="{'unfolded': unfolded}">
+      <div class="faq-item__question faq-question">
+        <div class="faq-question__image">
+          <i class="faq-question__image-icon"></i>
+        </div>
+        <p class="faq-question__text">
+          {{question}}
+        </p>
+        <button class="faq-question__collapse-btn" @click="unfolded = !unfolded"></button>
+      </div>
+      <div class="faq-item__answer faq-answer">
+        <div class="faq-answer__body">
+          <div class="faq-answer__text">
+            {{answer}}
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            unfolded: false
+        }
+    },
+    props: {
+        question: String,
+        answer: String,
+    }
+}
+</script>
---
+++ src/views/Faq.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="faq">
+      <h1 class="content-title">Профиль игрока</h1>
+      <div class="faq__list">
+        <FaqItem 
+            v-for="(qi, index) in questions" 
+            :key="index"
+            :question="qi.q"
+            :answer="qi.a"
+        />
+      </div>
+    </div>
+</template>
+
+<script>
+import FaqItem from "../components/faq/Faq-item"
+export default {
+    data() {
+        return {
+            questions: [
+                {
+                    q: "Lorem1",
+                    a: "LoremLorem1"
+                },
+                {
+                    q: "Lorem2",
+                    a: "LoremLorem2"
+                },
+                {
+                    q: "Lorem3",
+                    a: "LoremLorem3"
+                },
+                {
+                    q: "Lorem4",
+                    a: "LoremLorem4"
+                }
+            ]
+        }
+    },
+    components: {
+        FaqItem
+    }
+}
+</script>
